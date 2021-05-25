a935e83ca48315f1a7a5d97b7152ef0c905637ca
adasiek01 <81614226+adasiek01@users.noreply.github.com>
2021-05-25 14:44:46 +0200

    Range of board

--- ex_1.py
+++ ex_1.py
@@ -71,6 +71,23 @@ class Snake:
        self.x.append(-1)
        self.y.append(-1)
 
+    def out_of_x_left(self):
+        for i in range(0, self.size):
+            self.x[i] = 500
+
+    def out_of_x_right(self):
+        for i in range(0, self.size):
+            self.x[i] = -20
+
+    def out_of_y_up(self):
+        for i in range(0, self.size):
+            self.y[i] = 400
+
+    def out_of_y_down(self):
+        for i in range(0, self.size):
+            self.y[i] = -20
+
+
 
 class Game:
     def __init__(self):
@@ -81,27 +98,60 @@ class Game:
         self.chocolate = Chocolate(self.surface)
         self.chocolate.draw()
 
-    def play(self):
-        self.snake.creep()
-        self.chocolate.draw()
-        self.score()
-        pygame.display.flip()
-        if self.collision_finder(self.snake.x[0], self.snake.y[0], self.chocolate.x, self.chocolate.y):
-            self.snake.bigger()
-            self.chocolate.change_place()
-
-    def score(self):
-        font = pygame.font.SysFont('Helvetica', 22)
-        result = font.render(f"Score: {self.snake.size-1}", True, (0, 0, 205))
-        self.surface.blit(result, (420, 5))
-
     def collision_finder(self, a1, b1, a2, b2):
         if a1 >= a2 and a1 < a2 + 20:
             if b1 >= b2 and b1 < b2 + 20:
                 return True
         return False
 
+    def play(self):
+        self.snake.creep()
+        self.chocolate.draw()
+        self.score()
+        pygame.display.flip()
+        #poza planszą
+        for i in range(0, self.snake.size):
+            if self.snake.x[i] == -20 and self.snake.direction == "left":
+                self.snake.out_of_x_left()
+            if self.snake.x[i] == 500 and self.snake.direction == "right":
+                self.snake.out_of_x_right()
+            if self.snake.y[i] == -20 and self.snake.direction == "up":
+                self.snake.out_of_y_up()
+            if self.snake.y[i] == 400 and self.snake.direction == "down":
+                self.snake.out_of_y_down()
+
+        pygame.display.flip()
+        #zbieranie punktów
+        if self.collision_finder(self.snake.x[0], self.snake.y[0], self.chocolate.x, self.chocolate.y):
+            self.snake.bigger()
+            self.chocolate.change_place()
+
+        #koniec gry
+        for i in range(1, self.snake.size):
+            if self.collision_finder(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise
+
+    def score(self):
+        font = pygame.font.SysFont('Helvetica', 22)
+        result = font.render(f"Score: {self.snake.size-1}", True, (0, 0, 205))
+        self.surface.blit(result, (420, 5))
+
+    def the_end(self):
+        black = 0, 0, 0
+        self.surface.fill(black)
+        font = pygame.font.SysFont('Helvetica', 25)
+        message = font.render(f"You've lost, your final score is: {self.snake.size-1}", True, (255,255,0))
+        self.surface.blit(message, (100, 150))
+        message2 = font.render(f"Click 'p' to play again", True, (255, 255, 0))
+        self.surface.blit(message2, (100, 200))
+        pygame.display.flip()
+
+    def restart(self):
+        self.snake = Snake(self.surface, 1)
+        self.chocolate = Chocolate(self.surface)
+
     def go(self):
+        stop = False
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -109,20 +159,30 @@ class Game:
                 elif event.type == KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         quit()
-                    if event.key == K_UP or event.key == K_w:
-                        self.snake.move_up()
+                    if event.key == K_p:
+                        stop = False
+                    if not stop:
+                        if event.key == K_UP or event.key == K_w:
+                            self.snake.move_up()
 
-                    if event.key == K_DOWN or event.key == K_s:
-                        self.snake.move_down()
+                        if event.key == K_DOWN or event.key == K_s:
+                            self.snake.move_down()
 
-                    if event.key == K_LEFT or event.key == K_a:
-                        self.snake.move_left()
+                        if event.key == K_LEFT or event.key == K_a:
+                            self.snake.move_left()
 
-                    if event.key == K_RIGHT or event.key == K_d:
-                        self.snake.move_right()
-                    else:
-                        pass
-            self.play()
+                        if event.key == K_RIGHT or event.key == K_d:
+                            self.snake.move_right()
+                        else:
+                            pass
+
+            try:
+                if not stop:
+                    self.play()
+            except:
+                self.the_end()
+                stop = True
+                self.restart()
             time.sleep(0.1)
 
 
