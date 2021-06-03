0c10840904be34292d20379d684256cc43fdc984
adasiek01 <81614226+adasiek01@users.noreply.github.com>
2021-06-03 18:20:49 +0200

    great menu

--- menu.py
+++ menu.py
@@ -1,15 +1,14 @@
 from pygame_widgets import Button
-from function_ok import *
-import pygame_menu
+from easy import *
+from medium import *
+from hard import *
+
 
 
 def main_menu():
     def instructions():
-        pygame.init()
         gui_3 = pygame.display.set_mode((500, 400))
-        inst = pygame.image.load("inst.png")
-        gui_3.blit(inst, inst.get_rect())
-        pygame.display.flip()
+
 
         back_2_button = Button(
             gui_3, 30, 320, 100, 50, text='Back', textColour=(153, 0, 51), radius=40,
@@ -21,15 +20,57 @@ def main_menu():
         run = True
         while run:
             events = pygame.event.get()
+
+            gui_3.fill((102, 204, 255))
+            back_2_button.listen(events)
+            quit_button.listen(events)
+
+            red = (255, 0, 0)
+
+            X = 500
+            Y = 400
+
+            pygame.display.set_caption('Instruction')
+
+            font = pygame.font.Font('arial.ttf', 30)
+
+            text1 = font.render("Use 'w' or 'up arrow' to go up", False, red)
+            text2 = font.render("Use 's' or 'down arrow' to go down", False, red)
+            text3 = font.render("Use 'a' or 'left arrow' to go left", False, red)
+            text4 = font.render("Use 'd' or 'right arrow' to go right", False, red)
+            text5 = font.render("Use 'Enter' to pause the game", False, red)
+
+            textRect1 = text1.get_rect()
+            textRect2 = text2.get_rect()
+            textRect3 = text3.get_rect()
+            textRect4 = text4.get_rect()
+            textRect5 = text5.get_rect()
+
+            textRect1.center = (X // 2, Y // 8.5)
+            textRect2.center = (X // 2, Y // 4)
+            textRect3.center = (X // 2, Y // 2.5)
+            textRect4.center = (X // 2, Y // 1.8)
+            textRect5.center = (X // 2, Y // 1.4)
+
+
+            gui_3.blit(text1, textRect1)
+            gui_3.blit(text2, textRect2)
+            gui_3.blit(text3, textRect3)
+            gui_3.blit(text4, textRect4)
+            gui_3.blit(text5, textRect5)
+
+            back_2_button.draw()
+
+            quit_button.draw()
+
+            pygame.display.update()
+
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     run = False
                     quit()
 
-            gui_3.fill((102, 204, 255))
-            back_2_button.listen(events)
-            back_2_button.draw()
 
     def new_menu():
         pygame.init()
@@ -40,21 +81,21 @@ def main_menu():
             fontSize=50,
             inactiveColour=(255, 223, 128),
             pressedColour=(51, 204, 51),
-            onClick=function)
+            onClick=e_function)
 
         medium_button = Button(
             gui_2, 175, 160, 150, 50, text='Medium', textColour=(153, 0, 51), radius=40,
             fontSize=50,
             inactiveColour=(255, 223, 128),
             pressedColour=(51, 204, 51),
-            onClick=function)
+            onClick=m_function)
 
         hard_button = Button(
             gui_2, 200, 250, 100, 50, text='Hard', textColour=(153, 0, 51), radius=40,
             fontSize=50,
             inactiveColour=(255, 223, 128),
             pressedColour=(51, 204, 51),
-            onClick=function)
+            onClick=h_function)
 
         back_button = Button(
             gui_2, 30, 320, 100, 50, text='Back', textColour=(153, 0, 51), radius=40,
@@ -126,9 +167,6 @@ def main_menu():
         instructions_button.listen(events)
         instructions_button.draw()
         pygame.display.update()
-
-
-
 
 
 main_menu()
